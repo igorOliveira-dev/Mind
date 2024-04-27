b1d299edb89bc4b93f3dd89c83fb94eb8e051662
Igor <igor.oliveira.contact2210@gmail.com>
2024-04-27 12:50:31 -0300

    enviando projeto pra teste em diferentes telas

--- src/routes/saudeGeral/SaudeGeral.jsx
+++ src/routes/saudeGeral/SaudeGeral.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import Header from '../../elements/Header'
 import './saudeGeral.css'
+import { useNavigate } from 'react-router-dom';
 
-
+import logoPNG from '/logoPNG.png'
 import cerebroSaude from '/cerebro-saude.png'
 
 const SaudeGeral = () => {
@@ -12,13 +13,18 @@ const SaudeGeral = () => {
       window.scrollTo(0, 0);
     }, []);
 
+    const navigate = useNavigate();
+    const returnToInit = () => {
+      navigate("/");
+    }
+    
   return (
     <div>
       <Header />
       <div className="saudeGeralContent">
         <div className="initLeft">
           <h1>Saúde geral</h1>
-          <h2>A saúde mental com certeza também é influenciada pela saúde física, sabendo disso é aconselhável seguir uma dieta saudável, praticar exercícios e tomar outras medidas saudáveis, arraste para baixo para conhecer melhor sobre essas medidas!</h2>
+          <h2>A saúde física é com certeza interligada à saúde mental, uma pode afetar a outra e para ter uma saúde mental boa é importante manter a saúde física em dia, sabendo disso é aconselhável seguir uma dieta saudável, praticar exercícios e tomar outras medidas saudáveis, arraste para baixo para conhecer melhor sobre essas medidas!</h2>
         </div>
         <div className="initRight">
           <img src={cerebroSaude} alt="cérebro" />
@@ -36,25 +42,25 @@ const SaudeGeral = () => {
             </p>
           </p>
           <p>
-            <b>Alimentos Integrais e Saúde Mental</b>
+            <b>Alimentos Integrais e Saúde Mental:</b>
             <p>
               Alimentos integrais, como frutas, vegetais, grãos integrais e legumes, são ricos em fibras e antioxidantes. Eles ajudam a regular o açúcar no sangue e a inflamação, ambos ligados à saúde mental. Estudos mostram que dietas ricas em alimentos integrais podem reduzir o risco de depressão e ansiedade.
             </p>
           </p>
           <p>
-            <b>Açúcar e Saúde Mental</b>
+            <b>Açúcar e Saúde Mental:</b>
             <p>
               O consumo excessivo de açúcar tem sido associado a uma série de problemas de saúde, incluindo obesidade e doenças cardíacas. Além disso, estudos sugerem que dietas ricas em açúcar podem aumentar o risco de problemas de saúde mental. Reduzir o consumo de açúcar pode melhorar o humor e a energia.
             </p>
           </p>
           <p>
-            <b>Hidratação</b>
+            <b>Hidratação:</b>
             <p>
               A hidratação adequada é essencial para a saúde geral, incluindo a saúde mental. A desidratação pode afetar a concentração, o humor e a energia.
             </p>
           </p>
           <p>
-            <b>Conclusão</b>
+            <b>Conclusão:</b>
             <p>
               A alimentação saudável é uma parte crucial do cuidado com a saúde mental. Uma dieta equilibrada e nutritiva pode ajudar a melhorar o humor, aumentar a energia e reduzir o risco de problemas de saúde mental. Lembre-se, é sempre importante procurar aconselhamento de um profissional de saúde ao fazer mudanças significativas na dieta.
             </p>
@@ -64,8 +70,38 @@ const SaudeGeral = () => {
         <div className="exercicios">
           <h1>Exercícios</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque velit dolor, quisquam eos iste hic eius totam iure distinctio atque odit minus nam fugiat blanditiis quo. Est, numquam nesciunt!
+            A prática de exercícios traz diversas vantagens para a saúde física e mental, além de causar mudanças físicas que contibuem para o aumento da autoestima e autoeficácia. Por meio de exercícios, também vem mudanças biológicas, como a liberação de neurotransmissores que estão associados ao funcionamento do sistema nervoso central e ao cérebro.
           </p>
+          <p>
+            <b>Redução de sintomas de transtornos mentais:</b>
+            <p>
+              Evidências científicas indicam que a prática de exercícios previne sintomas de transtornos como depressão, ansiedade e transtorno bipolar. Exercícios aeróbicos e de resistencia são muito eficazes para isso quando são praticados por cerca de 90 minutos semanais.
+            </p>
+          </p>
+          <p>
+            <b>Recomendação da OMS:</b>
+            <p>
+              A Organização Mundial da Saúde (OMS) recomenda de 150 a 300 minutos de atividade aeróbica moderada a vigorosa por semana para adultos, e uma média de 60 minutos diários para crianças e adolescentes. Até mesmo pessoas sedentárias que pode não aguentar essa média, devem começar com as atividades possíveis para sair da condição de sedentário e ter uma boa saúde física.
+            </p>
+          </p>
+          <p>
+            <b>Tratamento de transtornos mentais com exercícios:</b>
+            <p>
+              Os exercícios em diversos casos na psicoterapia é usado como parte do tratamento de diversos transtornos mentais pela auta eficácia na diminuição de seus sintomas, isso prova que os exercícios tem resultado muito significativo na saúde mental.
+            </p>
+          </p>
+        </div>
+        <div className='outrosConselhos'>
+          <hr />
+          <div className="outrosConselhosContent">
+            <p>
+              Além dessas informações passadas, sempre é aconselhável ter todo o tipo de hábito que vai ajudar a melhorar sua saúde física e mental, alguns outros hábitos que podem te trazer vantagens são: rotinas de skin care, uma ótima higiente, passear com pets, sair com amigos, entre outros.
+            </p>
+            <div className="rightContent">
+              <img src={logoPNG} />
+              <button onClick={returnToInit}>Página inicial</button>
+            </div>
+          </div>
         </div>
     </div>
   )
